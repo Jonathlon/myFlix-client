e9545ca1bf06957f0d33aa3bda651767c88ce99a
Jon Heaney <113617821+Jonathlon@users.noreply.github.com>
2023-06-06 16:04:56 +0100

    add login/signup

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,13 +1,21 @@
 import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
+  const storedUser = localStorage.getItem("user");
+  const storedToken = localStorage.getItem("token");
+  const [user, setUser] = useState(storedUser ? JSON.parse(storedUser) : null);
+  const [token, setToken] = useState(storedToken ? storedToken : null);
   const [movies, setMovies] = useState([]);
-
   const [selectedMovie, setSelectedMovie] = useState(null);
 
   useEffect(() => {
+    if (!token) {
+      return;
+    }
     fetch("https://jonathlonmovieapp.herokuapp.com/movies")
       .then((response) => response.json())
       .then((data) => {
@@ -18,24 +26,41 @@ export const MainView = () => {
             ImagePath: movie.ImagePath,
             Description: movie.Description,
             Genre: {
-              Name: movie.Genre.Name
+              Name: movie.Genre.Name,
             },
             Director: {
               Name: movie.Director.Name,
-              Bio: movie.Director.Bio
+              Bio: movie.Director.Bio,
             },
-            Featured: movie.Featured.toString()
+            Featured: movie.Featured.toString(),
           };
         });
 
         setMovies(moviesFromApi);
+      });
+  }, [token]);
 
-    });
-  }, []);
+  if (!user) {
+    return (
+      <>
+        <LoginView
+          onLoggedIn={(user, token) => {
+            setUser(user);
+            setToken(token);
+          }}
+        />
+        or
+        <SignupView />
+      </>
+    );
+  }
 
   if (selectedMovie) {
     return (
-      <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
+      <MovieView
+        movie={selectedMovie}
+        onBackClick={() => setSelectedMovie(null)}
+      />
     );
   }
 
@@ -44,6 +69,13 @@ export const MainView = () => {
   }
   return (
     <div>
+      <button
+        onClick={() => {
+          setUser(null);
+        }}
+      >
+        Logout
+      </button>
       {movies.map((movie) => (
         <MovieCard
           key={movie._id}
